7d04114a19d049904ae4d4f87d74b6fd3a4a6906
Rezart Qelibari <qelibarr@informatik.uni-freiburg.de>
2016-08-06 23:26:16 +0200

    Modify testall.sh to fit new environment.

--- testall.sh
+++ testall.sh
@@ -1,22 +1,41 @@
 #!/bin/bash
-# Build from scratch and run unit tests from python 2.6, 2.7, 3.1
-# Then run doctests to verify doc examples
-# Note: requires nose installed in each python instance
+#############################################################################
+#	Pygonal
+#
+#	(c) 2016 Copyright Rezart Qelibari <qelibarr@informatik.uni-freiburg.de>
+#	Portions copyright (c) 2010 by Casey Duncan
+#	Portions copyright (c) 2009 The Super Effective Team
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+# 		http://www.apache.org/licenses/LICENSE-2.0
+#
+#	Unless required by applicable law or agreed to in writing, software
+#	distributed under the License is distributed on an "AS IS" BASIS,
+#	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#	See the License for the specific language governing permissions and
+#	limitations under the License.
+#
+#	See LICENSE.txt and CREDITS.txt
+#############################################################################
+
+
+# - Run unit tests from python 2.6, 2.7, 3.1
+# - Then run doctests to verify doc examples
 
 error=0
 
 rm -rf build
 
-for ver in 2.6 2.7 3.1; do
+for ver in 2.6 2.7 3 3.5; do
 	echo "************"
 	echo " Python $ver"
 	echo "************"
 	echo
 	if which python${ver}; then
-		# pass in -UNDEBUG to ensure assertions are enabled in C-extensions
-		SETUP_PY_CFLAGS="-UNDEBUG" python${ver} setup.py build && \
-		python${ver} -m nose.core \
-			-d -w build/lib.*${ver}/ --with-coverage --cover-erase $@ || error=1
+		python${ver} -m unittest pygonal.test  || error=1
 	else
 		echo >&2 "!!! Python ${ver} not found !!!"
 		error=1
@@ -26,6 +45,6 @@ done
 echo
 echo -n "Doctests... "
 srcdir=`pwd`
-cd build/lib.*3.?/ && python3 -m doctest ${srcdir}/doc/source/*.rst && echo "OK" || error=1
+python3 -m doctest ${srcdir}/doc/source/*.rst && echo "OK" || error=1
 
 exit $error
